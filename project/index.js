af76a23ced138a021da92d480deb5fe67503e7bb
AntonPanchenko GitHomePC <a.a.panchenkoo@gmail.com>
2019-11-16 22:19:29 +0200

    Регистрация #1
    Сделал не большую валидацию для регитсрации ошибки летят в консоль, и добавление данных в бд

--- project/index.js
+++ project/index.js
@@ -1,13 +1,41 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-const port = 7777;
-const server = app.listen(port);
+app.use(express.static('public'))
+const port = 7777
+const server = app.listen(port)
+const io = require('socket.io')(server)
+const md5 = require('md5')
+const mysql = require('mysql2')
 
-app.get('/', (req, res)=>{
-    res.render('index.ejs');
+app.get('/', (req, res) =>{
+    res.render('registration')
 })
-app.get('/registration', (req, res)=>{
-    res.render('registration.ejs');
+
+io.on('connection', (socket)=>{
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'dimpola',
+        password: '',
+    })
+    socket.on('new_user', (data)=>{
+        console.log('Create New User');
+        let hash = hashPassword(data.passwd);
+        const user = [data.login, data.name, data.email, hash.passwd, hash.soult];
+        const sql = 'INSERT INTO users(login, name, email, password, soult) VALUES(?,?,?,?,?)';
+        connection.query(sql, user, function (err, res) {
+            if(err) console.log(err)
+            else console.log('User add')
+        })
+    })
 })
+
+hashPassword = (password) =>{
+    let soult = +new Date();
+    let passwd = password.split('');
+    passwd.splice(3,0,soult);
+    passwd = passwd.join('');
+    passwd = md5(passwd);
+    return {passwd: passwd, soult: soult};
+}
--- project/public/js/reg.js
+++ project/public/js/reg.js
@@ -1,11 +1,40 @@
-document.getElementById('btn_create_account').addEventListener('click', e =>{
-    e.preventDefault();
-    let data = {
-        name: document.getElementById('user_name').value,
-        login: document.getElementById('user_login').value,
-        email: document.getElementById('user_email').value,
-        password: document.getElementById('user_email').value,
-        passwordConfirm: document.getElementById('user_password_confirm').value,
-    };
+const socket = io.connect("http://localhost:7777");
 
+console.log(socket)
+const userDataInputs = {
+    name: document.getElementById('user_name'),
+    login: document.getElementById('user_login'),
+    email: document.getElementById('user_email'),
+    passwd: document.getElementById('user_password'),
+    passwdConfirm: document.getElementById('user_password_confirm')
+};
+
+const regExps = {
+    name: /^[a-zA-Zа-яА-ЯёЁ'][a-zA-Z-а-яА-ЯёЁ' ]+[a-zA-Zа-яА-ЯёЁ']?$/ui,
+    login: /^[a-z]+([-_]?[a-z0-9]+){0,2}$/i,
+    email: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+};
+
+const send = document.getElementById('btn_create_account')
+const redirectToLoginPageBtn = document.getElementById('go_login')
+
+send.addEventListener('click', function (e) {
+    e.preventDefault();
+    if(!userDataInputs.name.value.match(regExps.name)){
+        return console.error('Invalid Name');
+    }
+    if(!userDataInputs.login.value.match(regExps.login)){
+        return console.error('Invalid login');
+    }
+    if(userDataInputs.passwd.value == '' || userDataInputs.passwdConfirm.value != userDataInputs.passwd.value){
+        return console.error('Invalid Password')
+    }
+    let data = {
+        socketID: socket.id,
+        name: userDataInputs.name.value,
+        login: userDataInputs.login.value,
+        email: userDataInputs.email.value,
+        passwd: userDataInputs.passwd.value,
+    }
+    socket.emit('new_user', data);
 })
